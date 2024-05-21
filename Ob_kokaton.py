ffa221bf77174fe63feb04660f4d2af8f2a3c996
c0a2313028 <130118601+c0a2313028@users.noreply.github.com>
2024-05-21 14:19:00 +0900

    初期状態

---
+++ Ob_kokaton.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed()
+        x = -1
+        y = 0
+        if key_lst[pg.K_UP]:
+            y = -1
+        elif key_lst[pg.K_DOWN]:
+            y = 1
+        elif key_lst[pg.K_RIGHT]:
+            x = 2
+        elif key_lst[pg.K_LEFT]:
+            x = -1
+        kk_rct.move_ip([x, y])
+        z = tmr%3200
+        screen.blit(bg_img, [-z, 0])
+        screen.blit(bg_img2, [-z+1600, 0])
+        screen.blit(bg_img, [-z+3200, 0])
+        screen.blit(bg_img2, [-z+4800, 0])
+        screen.blit(kk_img, kk_rct)
+        pg.display.update()
+        tmr += 1        
+        clock.tick(200)
+        
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
